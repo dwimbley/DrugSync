d9bf99df8aad5cf1cb2a8b7358ba72ad049d13e7
dwimbley <dwimbley@perimetersoftwaresolutions.com>
2021-04-24 21:01:03 -0400

    Initial checkin

---
+++ DrugSync.System/Class1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DrugSync.System
+{
+    public class DrugSyncer
+    {
+        public List<Drug> Sync(List<Drug> drugs)
+        {
+            var calculatedDrugs = new List<Drug>();
+
+            foreach (var item in drugs)
+            {
+                var endingDate = item.FillDate.AddYears(item.PlanYears);
+
+                var calculatedFillDate = item.FillDate;
+
+                while (calculatedFillDate < endingDate)
+                {
+                    calculatedFillDate = item.IsAnchor ? calculatedFillDate.AddDays(item.DaysSupply) : calculatedFillDate.AddDays(30).AddDays(item.FillBeforeDays * -1);
+
+                    var model = new Drug();
+                    model.IsAnchor = item.IsAnchor;
+                    model.DaysSupply = item.DaysSupply;
+                    model.FillBeforeDays = item.FillBeforeDays;
+                    model.AllowableDaysDifference = item.AllowableDaysDifference;
+                    model.Name = item.Name;
+                    model.FillDate = calculatedFillDate;
+                    calculatedDrugs.Add(model);
+                }
+            }
+
+            var anchorDrugs = calculatedDrugs.Where(m => m.IsAnchor);
+            var allOtherDrugs = calculatedDrugs.Where(m => !m.IsAnchor).OrderBy(m=>m.Name).ThenBy(m=>m.AnchorName);
+
+            Console.WriteLine("anchorDrugs.Count :: {0}", anchorDrugs.Count());
+
+            var combinedDrugs = new List<Drug>();
+
+            foreach (var drug in allOtherDrugs)
+            {
+                foreach (var item in anchorDrugs)
+                {
+                    var achorDate = item.FillDate;
+                    var daysDifference = (drug.FillDate - achorDate).Days;
+
+                    if (Math.Abs(daysDifference) > 7)
+                    {
+                        continue;
+                    }
+
+                    var isExactMatch = drug.FillDate == achorDate;
+                    var isFuzzyMatch = Math.Abs(daysDifference) <= drug.AllowableDaysDifference;
+
+                    var matchedDrug = new Drug();
+                    matchedDrug.Name = drug.Name;
+                    matchedDrug.FillDate = drug.FillDate;
+                    matchedDrug.IsAnchor = drug.IsAnchor;
+                    matchedDrug.DaysSupply = drug.DaysSupply;
+                    matchedDrug.FillBeforeDays = drug.FillBeforeDays;
+                    matchedDrug.AllowableDaysDifference = drug.AllowableDaysDifference;
+                    matchedDrug.Name = drug.Name;
+
+
+                   matchedDrug.IsMatchToAnchor = isExactMatch;
+                   matchedDrug.IsFuzzyMatchToAnchor = isExactMatch ? false : isFuzzyMatch;
+                   matchedDrug.AnchorName = item.Name;
+                   matchedDrug.DaysDifference = daysDifference;
+                    matchedDrug.ComparedDate = achorDate;
+
+                    combinedDrugs.Add(matchedDrug);
+                }
+            }
+
+            combinedDrugs.AddRange(anchorDrugs);
+
+            return combinedDrugs;
+        }
+    
+    }
+
+
+
+    public class Drug
+    {
+        public string Name { get; set; }
+        public bool IsAnchor { get; set; }
+        public DateTime FillDate { get; set; }
+        public int DaysSupply { get; set; }
+        public int FillBeforeDays { get; set; }
+        public int PlanYears { get; set; }
+        public int AllowableDaysDifference { get; set; }
+
+
+        public DateTime ComparedDate { get; set; }
+        public bool IsMatchToAnchor { get; set; }
+        public bool IsFuzzyMatchToAnchor { get; set; }
+        public string AnchorName { get; set; }
+        public int DaysDifference { get; set; }
+
+    }
+}
---
+++ DrungSync.Console/Program.cs
@@ -0,0 +1,198 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using DrugSync.System;
+
+namespace DrungSync.Console
+{
+    using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+
+            var drug1 = new Drug();
+            drug1.Name = "Rittalin";
+            drug1.IsAnchor = true;
+            drug1.DaysSupply = 30;
+            drug1.FillBeforeDays = 3;
+            drug1.PlanYears = 1;
+            drug1.AllowableDaysDifference = 4;
+            drug1.FillDate = new DateTime(2021, 5, 2);
+
+            var drug1a = new Drug();
+            drug1a.Name = "Adderall";
+            drug1a.IsAnchor = true;
+            drug1a.DaysSupply = 30;
+            drug1a.FillBeforeDays = 3;
+            drug1a.PlanYears = 1;
+            drug1a.AllowableDaysDifference = 4;
+            drug1a.FillDate = new DateTime(2021, 5, 21);
+
+
+            var drug2 = new Drug();
+            drug2.Name = "Oxycotin";
+            drug2.IsAnchor = false;
+            drug2.DaysSupply = 30;
+            drug2.FillBeforeDays = 3;
+            drug2.PlanYears = 1;
+            drug2.AllowableDaysDifference = 3;
+            drug2.FillDate = new DateTime(2021, 5, 25);
+
+            var drug3 = new Drug();
+            drug3.Name = "Vicadin";
+            drug3.IsAnchor = false;
+            drug3.DaysSupply = 30;
+            drug3.FillBeforeDays = 3;
+            drug3.PlanYears = 1;
+            drug3.AllowableDaysDifference = 4;
+            drug3.FillDate = new DateTime(2021, 4, 29);
+
+            var drug4 = new Drug();
+            drug4.Name = "Tramdaol";
+            drug4.IsAnchor = false;
+            drug4.DaysSupply = 20;
+            drug4.FillBeforeDays = 3;
+            drug4.PlanYears = 1;
+            drug4.AllowableDaysDifference = 3;
+            drug4.FillDate = new DateTime(2021, 6, 5);
+
+            var allDrugs = new List<Drug>();
+            allDrugs.Add(drug1);
+            allDrugs.Add(drug2);
+            allDrugs.Add(drug3);
+            allDrugs.Add(drug4);
+            allDrugs.Add(drug1a);
+            
+            var syncer = new DrugSyncer();
+            var output = syncer.Sync(allDrugs);
+
+            var fuzzyMatches = output.Where(m => !m.IsAnchor && m.IsFuzzyMatchToAnchor);
+            var matches = output.Where(m => !m.IsAnchor && m.IsMatchToAnchor);
+
+            var dateWithMostMatches = matches.Select(m => new { m.FillDate }).GroupBy(m=>m.FillDate).Select(m=> new
+            {
+                FillDate = m.Key,
+                Count = m.Count()
+            }).OrderByDescending(m=>m.Count).FirstOrDefault();
+
+            var dateWithMostFuzzyMatches = fuzzyMatches.Select(m => new { m.FillDate }).GroupBy(m=>m.FillDate).Select(m=> new
+            {
+                FillDate = m.Key,
+                Count = m.Count()
+            }).OrderByDescending(m=>m.Count).FirstOrDefault();
+
+
+            var drugsWithMatchDate = matches.Where(m => m.FillDate == dateWithMostMatches.FillDate).Count();
+            var drugsWithFuzzyMatchDate = fuzzyMatches.Where(m => m.FillDate == dateWithMostFuzzyMatches.FillDate).Count();
+
+            Console.WriteLine("Date with most matches {0}", dateWithMostMatches.FillDate);
+            Console.WriteLine("Date with most fuzzy matches {0}", dateWithMostFuzzyMatches.FillDate);
+
+            Console.WriteLine("drugsWithMatchDate Count :: {0}", drugsWithMatchDate);
+            Console.WriteLine("drugsWithFuzzyMatchDate Count :: {0}", drugsWithFuzzyMatchDate);
+            
+
+
+
+
+            Console.WriteLine("------------------------");
+            Console.WriteLine("-----");
+            Console.WriteLine("-----");
+            Console.WriteLine("-----        Anchor Drugs");
+            Console.WriteLine("-----");
+            Console.WriteLine("-----");
+            Console.WriteLine("------------------------");
+
+            foreach (var item in output.Where(m => m.IsAnchor))
+            {
+                Console.WriteLine(Environment.NewLine);
+                Console.WriteLine("Anchor Drug");
+                Console.WriteLine("Drug Name :: {0}", item.Name);
+                Console.WriteLine("Fill Date :: {0}", item.FillDate);
+                Console.WriteLine("*******************************");
+                Console.WriteLine(Environment.NewLine);
+            }
+
+            Console.WriteLine("------------------------");
+            Console.WriteLine("-----");
+            Console.WriteLine("-----");
+            Console.WriteLine("-----        Matched Drugs");
+            Console.WriteLine("-----");
+            Console.WriteLine("-----");
+            Console.WriteLine("------------------------");
+
+
+            foreach (var item in output.Where(m => !m.IsAnchor && m.IsMatchToAnchor))
+            {
+                Console.WriteLine(Environment.NewLine);
+                Console.WriteLine("NEW DRUG");
+                Console.WriteLine("Drug Name :: {0}", item.Name);
+                Console.WriteLine("Fill Date :: {0}", item.FillDate);
+                Console.WriteLine("Is match :: {0}", item.IsMatchToAnchor);
+                Console.WriteLine("Is fuzzy match :: {0}", item.IsFuzzyMatchToAnchor);
+                Console.WriteLine("Anchor Drug name :: {0}", item.AnchorName);
+                Console.WriteLine("Days difference :: {0}", item.DaysDifference);
+                Console.WriteLine("Compared Date :: {0}", item.ComparedDate);
+                Console.WriteLine("*******************************");
+                Console.WriteLine(Environment.NewLine);
+            }
+
+            Console.WriteLine("------------------------");
+            Console.WriteLine("-----");
+            Console.WriteLine("-----");
+            Console.WriteLine("-----        Fuzzy Match Drugs");
+            Console.WriteLine("-----");
+            Console.WriteLine("-----");
+            Console.WriteLine("------------------------");
+
+
+
+            foreach (var item in output.Where(m => !m.IsAnchor && m.IsFuzzyMatchToAnchor))
+            {
+                Console.WriteLine(Environment.NewLine);
+                Console.WriteLine("NEW DRUG");
+                Console.WriteLine("Drug Name :: {0}", item.Name);
+                Console.WriteLine("Fill Date :: {0}", item.FillDate);
+                Console.WriteLine("Is match :: {0}", item.IsMatchToAnchor);
+                Console.WriteLine("Is fuzzy match :: {0}", item.IsFuzzyMatchToAnchor);
+                Console.WriteLine("Anchor Drug name :: {0}", item.AnchorName);
+                Console.WriteLine("Days difference :: {0}", item.DaysDifference);
+                Console.WriteLine("Compared Date :: {0}", item.ComparedDate);
+                Console.WriteLine("*******************************");
+                Console.WriteLine(Environment.NewLine);
+            }
+
+
+            Console.WriteLine("------------------------");
+            Console.WriteLine("-----");
+            Console.WriteLine("-----");
+            Console.WriteLine("-----        Calculated Drugs");
+            Console.WriteLine("-----");
+            Console.WriteLine("-----");
+            Console.WriteLine("------------------------");
+
+
+
+
+            foreach (var item in output.Where(m => !m.IsAnchor && Math.Abs(m.DaysDifference) <= 7))
+            {
+                Console.WriteLine(Environment.NewLine);
+                Console.WriteLine("NEW DRUG");
+                Console.WriteLine("Drug Name :: {0}", item.Name);
+                Console.WriteLine("Fill Date :: {0}", item.FillDate);
+                Console.WriteLine("Is match :: {0}", item.IsMatchToAnchor);
+                Console.WriteLine("Is fuzzy match :: {0}", item.IsFuzzyMatchToAnchor);
+                Console.WriteLine("Anchor Drug name :: {0}", item.AnchorName);
+                Console.WriteLine("Days difference :: {0}", item.DaysDifference);
+                Console.WriteLine("Compared Date :: {0}", item.ComparedDate);
+                Console.WriteLine("*******************************");
+                Console.WriteLine(Environment.NewLine);
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
